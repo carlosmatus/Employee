0aef3a56094a1a515fde18d31ba12e8aef6e9644
carlosmatus <39808128+carlosmatus@users.noreply.github.com>
2019-02-20 20:41:30 -0600

    Update EmployeeRepositoryApplication.java

--- src/main/java/com/cm/kenzan/EmployeeRepositoryApplication.java
+++ src/main/java/com/cm/kenzan/EmployeeRepositoryApplication.java
@@ -1,39 +1,19 @@
 package com.cm.kenzan;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import com.cm.kenzan.exception.EmployeeNotFoundException;
-import com.cm.kenzan.service.EmployeeServiceImpl;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 @SpringBootApplication
-public class EmployeeRepositoryApplication implements CommandLineRunner{
+public class EmployeeRepositoryApplication {
 	private Logger logger = LoggerFactory.getLogger(this.getClass());
 	
-	@Autowired
-	private EmployeeServiceImpl employeeRepository;
 
 	public static void main(String[] args) {
-		
-		
+
 		SpringApplication.run(EmployeeRepositoryApplication.class, args);
 	}
-
-	@Override
-    public void run(String...args){
-
-       try {
-		System.out.println("empleado :"+employeeRepository.getEmployeeById(1L));
-	} catch (EmployeeNotFoundException e) {
-		// TODO Auto-generated catch block
-		System.out.println(e.getMessage());
-	}
-       
-    }
 }
 
